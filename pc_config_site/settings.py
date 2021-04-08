68994fc08176cfaabafcb52ace5a209ac52faffd
ydjin <e.mishchenko@chatme.ai>
2021-04-08 22:38:54 +0700

    implemented site_visualizer

--- pc_config_site/settings.py
+++ pc_config_site/settings.py
@@ -37,6 +37,8 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
+    'pc_configuration',
+    'site_visualizer'
 ]
 
 MIDDLEWARE = [
