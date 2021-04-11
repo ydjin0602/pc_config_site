f8699c7f6c0f6b93006839578d4d6eb0e476368d
ydjin <e.mishchenko@chatme.ai>
2021-04-11 16:24:38 +0700

    implemented post request in DBRequestsManagerView

--- db_requests_manager/views.py
+++ db_requests_manager/views.py
@@ -2,8 +2,10 @@ import json
 import logging
 import typing as t
 from dataclasses import dataclass
+from http import HTTPStatus
 
 from dataclasses_json import dataclass_json
+from django.db import IntegrityError
 from django.http import HttpRequest, JsonResponse, HttpResponseNotFound, HttpResponseBadRequest, HttpResponse
 from django.views.decorators.csrf import csrf_exempt
 from django.views.generic.base import View
@@ -70,7 +72,7 @@ class DBRequestsManagerView(View):
     def dispatch(self, request, *args, **kwargs):
         return super(DBRequestsManagerView, self).dispatch(request, *args, **kwargs)
 
-    def get(self, request: t.Union[GetConfigOptions, HttpRequest]):
+    def get(self, request: t.Union[GetConfigOptions, HttpRequest]) -> HttpResponse:
 
         if isinstance(request, HttpRequest):
             try:
@@ -86,7 +88,8 @@ class DBRequestsManagerView(View):
                 )
 
             except PCConfiguration.DoesNotExist as exception:
-                return self.__build_not_found_json_response(f'Configuration (token={request_body.token}) not found.')
+                return self.__build_not_found_json_response(f'Configuration (token={request_body.token}) not found.',
+                                                            exception)
 
         elif isinstance(request, GetConfigOptions):
             try:
@@ -94,23 +97,53 @@ class DBRequestsManagerView(View):
             except PCConfiguration.DoesNotExist as exception:
                 raise PCConfiguration.DoesNotExist from exception
 
-    def post(self, request: HttpRequest):
+    def post(self, request: HttpRequest) -> HttpResponse:
         try:
             request_body = self.__parse_create_config_request(request)
         except (json.JSONDecodeError, KeyError, UnicodeDecodeError, ValidationError) as exception:
             return self.__return_bad_request(exception)
 
-    def put(self):
+        try:
+
+            new_config = PCConfiguration(
+                token=request_body.token,
+                os_name=request_body.os.name,
+                os_version=request_body.os.version,
+                processor_name=request_body.processor.name,
+                processor_loading=request_body.processor.loading,
+                processor_architecture=request_body.processor.architecture,
+                processor_temperature=request_body.processor.temperature,
+                socket_info_host=request_body.socket_info.host,
+                socket_info_ip_address=request_body.socket_info.ip_address,
+                socket_info_mac_address=request_body.socket_info.mac_address,
+                disk_loading=request_body.disk.loading,
+                disk_memory=request_body.disk.memory,
+                ram=request_body.ram
+            )
+            new_config.save()
+        except IntegrityError as exception:
+            return self.__build_config_already_exist(f'Configuration (token={request_body.token}) already exist.',
+                                                     exception)
+
+        config = PCConfigurationSchema().dump(new_config)
+
+        return JsonResponse(
+            status=HTTPStatus.CREATED,
+            data=config
+        )
+
+    def put(self) -> HttpResponse:
         pass
 
-    def delete(self):
+    def delete(self) -> HttpResponse:
         pass
 
     def __parse_create_config_request(self, request: HttpRequest) -> CreateConfigOptions:
         return self.__CREATE_PC_CONFIG_SCHEMA.load(json.loads(request.body))
 
     @staticmethod
-    def __build_not_found_json_response(description: str) -> JsonResponse:
+    def __build_not_found_json_response(description: str, exception: t.Any) -> JsonResponse:
+        LOGGER.warning(description, exc_info=exception)
         return JsonResponse(
             status=HttpResponseNotFound.status_code,
             data={
@@ -119,7 +152,17 @@ class DBRequestsManagerView(View):
         )
 
     @staticmethod
-    def __return_bad_request(exception: t.Any):
+    def __build_config_already_exist(description: str, exception: t.Any) -> JsonResponse:
+        LOGGER.warning(description, exc_info=exception)
+        return JsonResponse(
+            status=HTTPStatus.CONFLICT,
+            data={
+                'error': description
+            }
+        )
+
+    @staticmethod
+    def __return_bad_request(exception: t.Any) -> JsonResponse:
         LOGGER.warning('Request body is invalid.', exc_info=exception)
         return JsonResponse(
             status=HttpResponseBadRequest.status_code,
