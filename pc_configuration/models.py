0b654c041f5406a0ef1b64ec182d26b166dab69b
Ydjin <akk-06@mail.ru>
2021-05-22 18:25:25 +0700

    added processor info in processor model

--- pc_configuration/models.py
+++ pc_configuration/models.py
@@ -13,6 +13,9 @@ class OSConfiguration(models.Model):
 class ProcessorConfiguration(models.Model):
     name = models.CharField('Processor Name', max_length=100)
     architecture = models.CharField('Processor Architecture', max_length=100, default=None)
+    total_cores = models.IntegerField('Total Cores', default=None)
+    max_frequency = models.CharField('Max Frequency', max_length=100, default=None)
+    current_frequency = models.CharField('Current Frequency', max_length=100, default=None)
     temperature = models.CharField('Processor Temperature', max_length=100, default=None)
     loading = models.CharField('Processor Loading', max_length=100, default=None)
 
