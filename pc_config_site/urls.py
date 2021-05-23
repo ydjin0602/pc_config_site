6e6ca984e1152f8cbc8918481e5b19d7c86dcade
ydjin <e.mishchenko@chatme.ai>
2021-05-23 18:28:38 +0700

    refactored static

--- pc_config_site/urls.py
+++ pc_config_site/urls.py
@@ -1,7 +1,9 @@
+from django.conf.urls.static import static
 from django.contrib import admin
 from django.urls import path
 
 from db_requests_manager.views import DBRequestsManagerView
+from pc_config_site import settings
 from site_visualizer.views import PageVisualizerView
 from token_generator.views import TokenGeneratorView
 
@@ -10,4 +12,4 @@ urlpatterns = [
     path('', PageVisualizerView.as_view(), name=''),
     path('db_manager/', DBRequestsManagerView.as_view(), name='db_manager'),
     path('token_generator/', TokenGeneratorView.as_view(), name='token_generator'),
-]
+] + static(settings.STATIC_URL, document_root=settings.STATIC_ROOT)
